bd15187caae67165876a80c99ff8365fc8d90491
raddevus <1radeutsch1@gmail.com>
2020-10-17 11:12:06 -0400

    Initial commit of project.

---
+++ js/main.js
@@ -0,0 +1,29 @@
+// Initialize Cloud Firestore through Firebase
+firebase.initializeApp({
+    apiKey: apiKey,
+    authDomain: 'corecodoc.firebaseapp.com',
+    projectId: 'corecodoc'
+  });
+  
+  var db = firebase.firestore();
+  console.log(db);
+
+db.collection("users").add({
+    first: "Ada",
+    last: "Lovelace",
+    born: 1815
+})
+.then(function(docRef) {
+    console.log("Document written with ID: ", docRef.id);
+})
+.catch(function(error) {
+    console.error("Error adding document: ", error);
+});
+
+function uuidv4() {
+    // got this from https://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid
+    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    );
+}
+
