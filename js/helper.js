e405d3dc19a8ab497b84f4c6ac08af0ea8e11533
raddevus <1radeutsch1@gmail.com>
2020-10-18 15:45:32 -0400

    Added dealing with others competencies.

--- js/helper.js
+++ js/helper.js
@@ -26,12 +26,34 @@ const selfManagementCompetencies = [
     // new Competency("sm-04","Flexibility","Openness to different and new ways of doing things; willingness to modify one’s preferred way of doing things.")
 ];
 
+const dealingWithOthersCompetencies = [
+    new Competency({value:"do-01",text:"Establishing focus",description:"The ability to develop and communicate goals in support of the company's mission."}),
+    new Competency({value:"do-02",text:"Providing emotional support",description:"The ability to enhance other people's commitment to their work."}),
+    new Competency({value:"do-03",text:"Fostering teamwork",description:"As a team member, the ability and desire to work cooperatively with others on a team; as a team leader, the ability to demonstrate interest, skill, and success in getting groups to learn to work together."}),
+    new Competency({value:"do-04",text:"Empowering others",description:"The ability to convey confidence in employees’ ability to be successful, especially at challenging new tasks; delegating significant responsibility and authority; allowing employees freedom to decide how they will accomplish their goals and resolve issues."}),
+    new Competency({value:"do-05",text:"Managing change",description:"The ability to demonstrate support for innovation and for organizational changes needed to improve the organization’s effectiveness; initiating, sponsoring, and implementing organizational change; helping others to successfully manage organizational change."}),
+    new Competency({value:"do-06",text:"Developing others",description:"The ability to delegate responsibility and to work with others and coach them to develop their capabilities."}),
+    new Competency({value:"do-07",text:"Managing performance",description:"The ability to take responsibility for one’s own or one’s employees’ performance, by setting clear goals and expectations, tracking progress against the goals, ensuring feedback, and addressing performance problems and issues promptly."}),
+    new Competency({value:"do-08",text:"Attention to communication",description:"The ability to ensure that information is passed on to others who should be kept informed."}),
+    new Competency({value:"do-09",text:"Oral communication",description:"The ability to express oneself clearly in conversations and interactions with others."}),
+    new Competency({value:"do-10",text:"Written communication",description:"The ability to express oneself clearly in business writing."}),
+    new Competency({value:"do-11",text:"Persuasive commuication",description:"The ability to plan and deliver oral and written communications that make an impact and persuade their intended audiences."}),
+    new Competency({value:"do-12",text:"Interpersonal awareness",description:"The ability to notice, interpret, and anticipate others’ concerns and feelings, and to communicate this awareness empathetically to others."}),
+    new Competency({value:"do-13",text:"Influencing others",description:"The ability to gain others’ support for ideas, proposals, projects, and solutions."}),
+    new Competency({value:"do-14",text:"Building collarborative relationships",description:"The ability to develop, maintain, and strengthen partnerships with others inside or outside the organization who can provide information, assistance, and support."}),
+    new Competency({value:"do-15",text:"Customer orientation",description:"The ability to demonstrate concern for satisfying one’s external and/or internal customers."}),
+    // new Competency({value:"do-16",text:"",description:""}),
+    // new Competency({value:"do-17",text:"",description:""}),
+    // new Competency({value:"do-18",text:"",description:""}),
+]
+
 function setCompetencySelection(){
     var cgValue = $("#competencyGroup").val();
     if (cgValue == "00-00"){
         // the blank item was chosen, nothing to do.
         document.querySelector("#competency").value = "00-00";
         $("#competencyDescription").text("");
+        $("#competency").empty();
         return;
     }
     $("#competency").empty();
@@ -51,6 +73,12 @@ function setCompetencySelection(){
             break;
         }
         case "cg-02" :{
+            dealingWithOthersCompetencies.map((item) => {
+                var localOption = new Option(item.text, item.value+"^"+item.description, false, false);
+                console.log(localOption);
+                $("#competency").append($(localOption));
+                console.log(item.value + " : " +  item.text);
+            });
             break;
         }
         case "cg-03":{
