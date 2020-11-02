3192934dd8cce4ef0525466bf5b9eb6d041c77a1
raddevus <1radeutsch1@gmail.com>
2020-11-02 17:11:03 -0500

    Continuing work on formatting the journal report.  Looks much better and now the code that generates the html nodes is much cleaner.

--- js/journalTab.js
+++ js/journalTab.js
@@ -17,32 +17,33 @@ function addEntries(){
      )
 
     for (var x = 0;x < localJournal.entries.length;x++){
-        // NOTE: gen-chk is my special value used for
-        // my script to determine which items are checked.
-        // console.log(examples[x]);
-
-        $("#entries").append($(document.createElement("div")).prop({
-            class:"",
-            text:localJournal.entries[x].created,
-            // }).append(
-            //     $(document.createElement("input")).prop({
-            //     id: "example-"+x,
-            //     name: "interest",
-            //     value: x,
-            //     type: "checkbox",
-            //     class:"form-check-input gen-chk",
-            //     })
-            // ).append(
-            //     $(document.createElement("label")).prop({
-            //     for: "example-"+x,
-            //     }).html(examples[x])
-        })
-        .append(localJournal.entries[x].competency)
-        .append(localJournal.entries[x].created)
-        //.append(document.createElement("div"))
-        .append(document.createElement("br"))
-        .append(localJournal.entries[x].notes)
+        // 1. Add outer div to "#entries div"
+        $("#entries").append($(document.createElement("div"))
+        // 2. append strong text to div which contains competency name
+        .append($(document.createElement("strong")).text(localJournal.entries[x].competency))
+        // 3. append span add text which includes formatted created date and 
+        //    add a class to the span (for styling purposes)
+        .append($(document.createElement("span"))
+            .text(formatDate(localJournal.entries[x].created))
+            .prop({class:"createdDate"}))
+        .append($(document.createElement("p")).text(localJournal.entries[x].notes))
         .append($(document.createElement("h5")).text("Supporting Examples") )
       );
     }
+}
+
+// This helper formatDate function was obtained from : 
+// https://stackoverflow.com/questions/23593052/format-javascript-date-as-yyyy-mm-dd
+function formatDate(date) {
+    var d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2) 
+        month = '0' + month;
+    if (day.length < 2) 
+        day = '0' + day;
+
+    return [year, month, day].join('-');
 }
